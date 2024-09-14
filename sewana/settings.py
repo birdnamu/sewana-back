04af5b1c9aa65fb4750d3c5e48909d8f87d00560
Hyorim K <37091594+daydeiday@users.noreply.github.com>
2024-09-14 18:08:46 +0900

    STATIC_URL = 'static/'에서 '/static/'으로 변경..

--- sewana/settings.py
+++ sewana/settings.py
@@ -274,7 +274,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = Path(BASE_DIR) / 'staticfiles'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
